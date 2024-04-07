c46a2c6aadf7a28bcb03665817e66d0367f93ad7
Sednice13 <sa224pb@student.lnu.se>
2024-04-07 19:23:42 +0200

    staring with client for project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as ReactDOMClient from 'react-dom/client';
+
+
+const container = document.getElementById('root');
+
+
+const root = ReactDOMClient.createRoot(container);
+
+
+root.render(<App  />);
+
+
+
+//reactDom.render(<App/> , document.getElementById('root'))
