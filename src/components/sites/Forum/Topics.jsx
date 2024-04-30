335e6d36eb70503faff0506c3169e34419b0c53b
Sednice13 <sa224pb@student.lnu.se>
2024-04-30 14:05:30 +0200

    make so there are three main topics

--- src/components/sites/Forum/Topics.jsx
+++ src/components/sites/Forum/Topics.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import styles from './styles/ChooseForum.module.css';  
+import { Fade, Slide } from "react-awesome-reveal";
+
+const topicsInfo = [
+  { title: "Musik", description: "Diskussioner om melodier, genrer och musikhistoria." },
+  { title: "Artister", description: "Allt om dina favoritartister och band." },
+  { title: "Allmänt", description: "Övriga ämnen och allmänna diskussioner." }
+];
 
 const Topics = () => {
-  let { topic } = useParams(); 
+  let { topic } = useParams();
 
   return (
-    <div>
-      <h1>{topic}</h1>  {/* Visa vilket ämne som valts */}
+    <div className={styles.topicsContainer}>
+        <Slide direction="left" duration={1500}> <h1>{topic}</h1>  </Slide>
+     
+      <div className={styles.topicsGrid}>
+        <Slide direction="right" duration={1800}> 
+        {topicsInfo.map((item, index) => (
+          <div key={index} className={styles.topicBox}>
+            <Fade direction="right" duration={2000}>    
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            </Fade>
+          </div>
+        ))}
+        </Slide>
+      </div>
     </div>
   );
 }
